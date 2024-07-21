a8aacac999b5e2e0056710208b37ea7545419071
abraham <abrahamtikuye@gmail.com>
2024-07-21 18:45:39 +0300

    component2

--- App.js
+++ App.js
@@ -1,96 +1,19 @@
-// App.js
-
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { StatusBar } from 'expo-status-bar';
-
-export default function App() {
-  return (
-    <LinearGradient
-      colors={['#FF6F91', '#D83357']}
-      style={styles.wrapper}
-    >
-      <SafeAreaView style={styles.wrapper}>  
-      <View style={styles.container} >
-          <Text style={styles.temp}>6</Text>
-          <Text style={styles.feels}>Feels Like 5</Text>
-          <View style={styles.highlowwrapper}>
-            <Text style={styles.highlow}>High : 8</Text>
-            <Text style={styles.highlow}>Low: 7</Text>
-          </View>
-        </View>
-        <View style={styles.bodywrapper}>
-          <Text style={styles.bodyText}>It's sunny</Text>
-          <Text style={styles.bodyText}>It's perfect T-shirt weather</Text>
-          <TouchableOpacity style={styles.buttonContainer}>
-            <LinearGradient
-              colors={['#4c669f', '#3b5998', '#192f6a']}
-              style={styles.button}
-            >
-              <Text style={styles.buttonText}>Press Me</Text>
-            </LinearGradient>
-          </TouchableOpacity>
-        </View>
-        {/* <StatusBar style='autoo'></StatusBar> */}
-      </SafeAreaView>
-    </LinearGradient>
-  );
+import { View, StyleSheet } from "react-native";
+import Current from "./src/Current";
+import React from "react";
+import UpcomingWeather from "./src/Upcomingweather";
+const App =()=>{
+  return(
+<View style={styles.container}>
+<UpcomingWeather/>
+  {/* <Current/> */}
+</View>
+  )
 }
+const styles= StyleSheet.create({
+container:{
+  flex:1
+}
+})
 
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-  },
-  autoo:{
-    backgroundColor:'blue'
-  },
-  container: {
-    alignItems: 'center',
-    flex: 1,
-  },
-  temp: {
-    fontSize: 48,
-    color: 'white',
-  },
-  feels: {
-    fontSize: 40,
-    color: 'white',
-  },
-  highlowwrapper: {
-    flexDirection: 'row',
-  },
-  highlow: {
-    fontSize: 20,
-    color: 'white',
-  },
-  bodywrapper: {
-    alignItems: 'center',
-    padding: 20,
-  },
-  bodyText: {
-    fontSize: 16,
-    color: '#fff',
-  },
-  gradientTextWrapper: {
-    alignItems: 'center',
-  },
-  transparentText: {
-    backgroundColor: 'black',
-  },
-  buttonContainer: {
-    marginTop: 20,
-    borderRadius: 25,
-  },
-  button: {
-    paddingVertical: 15,
-    paddingHorizontal: 25,
-    borderRadius: 25,
-    alignItems: 'center',
-  },
-  buttonText: {
-    fontSize: 16,
-    color: '#fff',
-    textAlign: 'center',
-  },
-});
+export default App; 
